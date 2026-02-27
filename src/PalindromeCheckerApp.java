9bd99c55a939e6c52416b2ef0df5fe6677265e7e
ArushiLakhera30 <al0448@srmist.edu.in>
2026-02-27 15:37:34 +0530

    Character Array Based Palindrome Check

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -3,17 +3,20 @@ import java.util.Scanner;
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
         // Hardcoded string to check
-        String text = "madam";
+        String text = "racecar";
 
         // Clean the string: remove spaces and make lowercase
         String cleaned = text.replaceAll("\\s+", "").toLowerCase();
 
+        // Convert to character array
+        char[] chars = cleaned.toCharArray();
+
         boolean isPalindrome = true;
         int left = 0;
-        int right = cleaned.length() - 1;
+        int right = chars.length - 1;
 
         while (left < right) {
-            if (cleaned.charAt(left) != cleaned.charAt(right)) {
+            if (chars[left] != chars[right]) {
                 isPalindrome = false;
                 break;
             }
